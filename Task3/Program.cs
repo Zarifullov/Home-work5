ff4a28353a5c0b2856864fd3ad7dbfedbeeb75dd
Renat <rzarifullov@gmail.com>
2022-07-31 22:57:38 +0300

    Task3 added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,3 +2,34 @@
 
 // [3 7 22 2 78] -> 76
 
+double[] array = new double[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(0, 10); 
+  System.Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным значением = ");
+
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < 4; i++)
+    {
+    if (max <= array[i])
+    {
+        max = array[i];
+    }
+    
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+    }
+double result = max - min;
+
+Console.WriteLine(result);
+
